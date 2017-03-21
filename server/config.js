0d233a0733fd2fb0464adb2f386f30f0d3ba1522
Carlos Escobar <carlos.b.escobar@gmail.com>
2017-03-21 03:01:11 -0400

    refactoring and cleanup

---
+++ server/config.js
@@ -0,0 +1,19 @@
+// Twilio Credentials and configs
+//  TODO: all these id's and phone numbers should be in configs/env vars
+module.exports = {
+    ACCOUNT_SID : 'ACcf879ae1f3f137d3d69338409cdeb851',
+    AUTH_TOKEN : 'a5d7215fab103ce490398df2f503005b',
+    TWILIO_PHONE : '+16175531502',
+
+    // scheduling configs
+    // runs the schedule every monday at 12:00pm
+    // https://bunkat.github.io/later
+    SCHEDULE_TEXT : 'every Monday at 12:00pm',
+
+    // uncomment this line to see the scheduling fire more often
+    // SCHEDULE_TEXT : 'every 5 sec',
+    server: {
+        port: 1337,
+        host: 'localhost'
+    }
+};
---
+++ server/mock-db/users.js
@@ -0,0 +1,31 @@
+const generateId = require('../utils/generateId');
+
+module.exports = [
+    {
+        name: 'Carlos Escobar',
+        id: generateId(),
+        phone: '555-555-5555',
+        phoneFormatted: '+15555555555',
+        primary: true,
+        backup: false,
+        order: 1
+    },
+    {
+        name: 'Joe the Developer',
+        id: generateId(),
+        phone: '555-555-5555',
+        phoneFormatted: '+15555555555',
+        primary: false,
+        backup: true,
+        order: 2
+    },
+    {
+        name: 'Sam the Developer',
+        id: generateId(),
+        phone: '555-555-5555',
+        phoneFormatted: '+15555555555',
+        primary: false,
+        backup: false,
+        order: 3
+    }
+];
